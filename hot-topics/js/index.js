9eb87974bf0db6435563213a8261a3d654a4b73a
JessJacks11 <46456000+JessJacks11@users.noreply.github.com>
2019-04-19 19:45:16 -0400

    added hot-topics and assets

---
+++ hot-topics/js/index.js
@@ -0,0 +1,26 @@
+const navItems = document.querySelectorAll("header .header-container nav ul li a");
+const container = document.querySelector(".content-container");
+
+let path = "http://localhost/mtm1526/hot-topics/partials/home.html";
+
+function handleClick(ev) {
+    ev.preventDefault();
+    path = ev.target.href;
+    handleAjax(path);
+
+}
+
+function handleAjax(urlValue) {
+    fetch(urlValue)
+        .then(function (rsp) {
+            return rsp.text();
+        })
+        .then(function (data) {
+            container.innerHTML = data;
+        })
+}
+
+handleAjax(path);
+
+navItems[0].addEventListener("click", handleClick);
+navItems[1].addEventListener("click", handleClick);
